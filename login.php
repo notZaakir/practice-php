fac58543251e01e9e4c43d2a9ca5ae94b85cb8cf
Sergio Duchaine <106310981+Sergio-Duchaine@users.noreply.github.com>
2023-06-22 14:28:32 +0200

    login with bootstrap not testes

--- login.php
+++ login.php
@@ -32,8 +32,10 @@
  }
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -43,39 +45,62 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
     <script src="loginvalidate.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
-    <title>Login</title> 
+    <title>Login</title>
+    <style>
+        body {
+            background-color: #343a40;
+        }
+
+        h1,
+        label,
+        a {
+            color: white;
+        }
+
+        .btn {
+            background-color: black;
+            color: white;
+        }
+
+        .error {
+            color: red;
+            font-style: italic;
+        }
+    </style>
 </head>
+
 <body>
-    
 
-    
-  
+    <div class="container">
+        <form method="POST" id="signup" class="mt-5">
+            <h1 class="text-center">Login</h1>
+            <div class="form-container">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" name="email" id="email" class="form-control"
+                    value="<?= htmlspecialchars($_POST["email"] ?? "") ?>">
+                <div class="error"></div>
+            </div>
 
-    <form method = "POST" id = signup>
-    <h1>Login</h1>
-    <div class="input-control">
-        <label for = "email" >Email</label>
-        <input type="email" name="email" id="email"
-                            value="<?= htmlspecialchars($_POST["email"] ?? "") ?>">
-                            <div class="error"></div>
+            <div class="form-container">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" name="password" id="password" class="form-control">
+                <div class="error"></div>
+            </div>
+
+            <?php if ($is_invalid): ?>
+                <div class="error">Username or password incorrect</div>
+            <?php endif; ?>
+
+            <button type="submit" class="btn btn-primary">Log in</button>
+            <p class="mt-3">Don't have an account?</p>
+            <a href="signup.html" style="color: white;">Sign up</a>
+        </form>
     </div>
 
-        
-     <div class="input-control">
-        <label for = "password" >Password</label>
-        <input type="password" name="password" id="password">
-        <div class="error"></div>
-     </div>
-
-     <?php if ($is_invalid): ?>
-        <div style= "color: red; font-style : italic;" >Username or password incorrect</div>
-    <?php endif; ?>
-        <button>Log in</button>
-        <p>Dont have an account? </p>
-        <a href="signup.html">Sign up</a> 
-
-
-    </form>
+    <script src="loginvalidate.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
